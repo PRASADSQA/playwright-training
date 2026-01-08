7de33357f7d9717f14b75bed8cadbfec95e2f9c9
Prasad Ranasinghe <janendrar@gmail.com>
2026-01-08 20:40:36 +0100

    Initial commit

--- tests/day1-first.spec.ts
+++ tests/day1-first.spec.ts
@@ -5,3 +5,13 @@ test('Example site loads', async ({ page }) => {
   await expect(page).toHaveTitle(/Example Domain/);
 });
 
+test('Verify example.com body text', async ({ page }) => {
+  await page.goto('https://example.com/');
+
+  const body = page.locator('body');
+
+  await expect(body).toContainText(
+    'This domain is for use in documentation examples without needing permission'
+  );
+});
+
